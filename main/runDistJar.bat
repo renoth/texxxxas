4b06e7683f2d089dbcff748c1911348c249a70b6
renoth <johannes.renoth@gmx.de>
2015-05-10 12:16:04 +0200

    added executor for windows / dump save file in working dir

---
+++ main/runDistJar.bat
@@ -0,0 +1,1 @@
+start cmd /k java -jar build\dist\texxxxas-main-0.0.1.jar
--- main/src/main/java/de/texxxxas/main/TexxxxasApplication.java
+++ main/src/main/java/de/texxxxas/main/TexxxxasApplication.java
@@ -6,11 +6,16 @@ import de.texxxxas.common.TexxxxasGame;
 import de.texxxxas.common.game.GameParameters;
 import de.texxxxas.generator.TexxxxasGenerator;
 import de.texxxxas.ui.TexxxxasUi;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
 
 public class TexxxxasApplication {
+
+    static Logger log = LoggerFactory.getLogger(TexxxxasApplication.class);
+
     public static void main(String[] args) {
         System.out.println("\nStarting Application: \n");
 
@@ -31,7 +36,7 @@ public class TexxxxasApplication {
         ObjectMapper mapper = new ObjectMapper();
         mapper.configure(SerializationFeature.INDENT_OUTPUT, true); //pretty output
         try {
-            mapper.writeValue(new File("game.json"), game);
+            mapper.writeValue(new File(System.getProperty("user.dir") + "\\game.json"), game);
         } catch (IOException e) {
             e.printStackTrace();
         }
